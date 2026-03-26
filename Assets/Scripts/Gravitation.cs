a8dee82ad01767e3f67e7752afe696ae6f7c3826
pariyakorn saelao <pariyakorn.carrot28@gmail.com>
2026-03-26 11:10:07 +0700

    Add Orbiting Function

--- Assets/Scripts/Gravitation.cs
+++ Assets/Scripts/Gravitation.cs
@@ -1,23 +1,37 @@
 using UnityEngine;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class Gravitation : MonoBehaviour
 {
-
+    //Create a List objects in the galaxy to attract
     public static List<Gravitation> otherObj;
     private Rigidbody rb;
     const float G = 6.67f;
+
+    //set speed fot orbiting
+    [SerializeField] bool planet = false; //if not a planet -> orbit
+    [SerializeField] int orbitSpeed = 1000000;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
+        //Create a List for the first time
         rb = GetComponent<Rigidbody>();
         if (otherObj == null )
         {
             otherObj = new List<Gravitation>();
         }
+
+        //Add object (with gravitation script) to attract to the list
         otherObj.Add( this );
+
+        //orbiting
+        if ( !planet )
+        {
+            rb.AddForce(Vector3.left * orbitSpeed);
+        }
     }
 
     // Update is called once per frame
